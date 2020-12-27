f4a6bdf65bd2e2584eeb2353431984fa5439112d
avi turkewitz <aviturkewitz@gmail.com>
2020-12-27 15:28:21 -0500

    sending messages to the tab

--- content_scripts/set_score.js
+++ content_scripts/set_score.js
@@ -1,1 +1,20 @@
-console.log("I have run on the tab")
+console.log("I have run on the tab");
+
+(function() {
+  if (window.hasRun) {
+    return;
+  }
+  window.hasRun = true;
+
+  /**
+   * Listen for messages from the background script.
+   * Call "beastify()" or "reset()".
+  */
+  browser.runtime.onMessage.addListener((message) => {
+    if (message.command === "set_score") {
+      console.log("LOGGING MESSSGE:");
+      console.log(message);
+    }
+  });
+
+})();
--- popup/select_score.js
+++ popup/select_score.js
@@ -1,4 +1,29 @@
-const container = document.getElementById("popup-content")
-container.className = "red-border"
+const logError = (error) => {
+  console.error("An error occurred", error);
+}
+
+function listenForSubmit() {
+  const container = document.getElementById("popup-content")
+  container.className = "red-border"
+  const submit = document.getElementById("js--submit")
+
+  submit.addEventListener("click", () => {
+    const criticScoreElem = document.getElementById("js--critic_score")
+    const audienceScoreElem = document.getElementById("js--audience_score")
+
+    const sendMessage = (tabs) => {
+      browser.tabs.sendMessage(tabs[0].id, {
+        command: "set_score",
+        criticScore: criticScoreElem.value,
+        audienceScore: audienceScoreElem.value
+      });
+    }
+
+    browser.tabs.query({active: true, currentWindow: true})
+      .then(sendMessage)
+  })
+}
 
 browser.tabs.executeScript({file: "/content_scripts/set_score.js"})
+.then(listenForSubmit)
+.catch(logError)
